9f84db94e212a8c4f1d33e8a02cde58709f1b8f2
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-22 11:10:45 +0400

    pressable

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,15 +1,39 @@
-import { Text, View, Image, ImageBackground, ScrollView, Button } from "react-native";
+import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Press"
-        onPress={() => console.log("Button pressed")}
-        color="green"
-        disabled
-      />
+      <Pressable
+        onPress={() => {
+          console.log("Image pressed");
+        }}
+      >
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+      <Pressable
+        onPress={() => {
+          console.log("Text pressed");
+        }}
+      >
+        <Text>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum, nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget
+          aliquam nisl nunc sit amet nisl. Donec condimentum, nisl eu ultricies
+          ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet
+          nisl. Donec condimentum, nisl eu ultricies ultricies, nunc nisl
+          aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum,
+          nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget aliquam nisl
+          nunc sit amet nisl. Donec condimentum, nisl eu ultricies ultricies,
+          nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec
+          condimentum, nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget
+          aliquam nisl nunc sit amet nisl. Donec condimentum, nisl eu ultricies
+          ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet
+          nisl. Donec condimentum, nisl eu ultricies ultricies, nunc nisl
+          aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum,
+          nisl eu ultricies
+        </Text>
+      </Pressable>
     </View>
   );
 }
