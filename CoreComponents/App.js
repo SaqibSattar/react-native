30eb4ba1720007d1434216b53bc4da7b7d5ec709
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-22 10:48:48 +0400

    scroll view

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,17 +1,38 @@
-import { Text, View, Image, ImageBackground } from "react-native";
+import { Text, View, Image, ImageBackground, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+    <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          condimentum, nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget
+          aliquam nisl nunc sit amet nisl. Donec condimentum, nisl eu ultricies
+          ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet
+          nisl. Donec condimentum, nisl eu ultricies ultricies, nunc nisl
+          aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum,
+          nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget aliquam nisl
+          nunc sit amet nisl. Donec condimentum, nisl eu ultricies ultricies,
+          nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec
+          condimentum, nisl eu ultricies ultricies, nunc nisl aliquam nunc, eget
+          aliquam nisl nunc sit amet nisl. Donec condimentum, nisl eu ultricies
+          ultricies, nunc nisl aliquam nunc, eget aliquam nisl nunc sit amet
+          nisl. Donec condimentum, nisl eu ultricies ultricies, nunc nisl
+          aliquam nunc, eget aliquam nisl nunc sit amet nisl. Donec condimentum,
+          nisl eu ultricies
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
       {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
       <Image
         source={{ uri: "https://picsum.photos/200" }}
         style={{ width: 300, height: 300 }}
-      /> */}
+      />
       <ImageBackground source={logoImg} style={{ flex: 1 }}>
         <Text>Image text</Text>
-      </ImageBackground>
+      </ImageBackground> */}
+      </ScrollView>
     </View>
   );
 }
