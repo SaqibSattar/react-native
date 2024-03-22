f1591553f2ad12bf3da74cb4bf4600cd5a73a98e
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-22 11:24:24 +0400

    status bar

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,31 +1,11 @@
 import { useState } from "react";
-import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable, Modal } from "react-native";
+import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button
-        title="Press"
-        onPress={() => setIsModalVisible(true)}
-        color="midnightblue"
-      />
-      <Modal
-        visible={isModalVisible}
-        onRequestClose={() => setIsModalVisible(false)}
-        animationType="slide"
-        presentationStyle="formSheet"
-      >
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text>Modal content</Text>
-          <Button
-            title="Close"
-            onPress={() => setIsModalVisible(false)}
-            color="midnightblue"
-          />
-        </View>
-      </Modal>
+      <StatusBar barStyle="dark-content" backgroundColor="lightblue" hidden />
     </View>
   );
 }
