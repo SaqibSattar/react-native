f61a07ad5911aeaaa7cf4661fa89a825f376be98
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-23 10:25:33 +0400

    alert

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,14 +1,11 @@
 import { useState } from "react";
-import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator } from "react-native";
+import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-    <ActivityIndicator />
-    <ActivityIndicator size="large" />
-    <ActivityIndicator size="large" color="midnightblue" />
-    <ActivityIndicator size="large" color="midnightblue" animating={false} />
+      <Button title="Press" onPress={() => Alert.alert("Invalid data!")} />
     </View>
   );
 }
