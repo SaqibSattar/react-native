cc179c810d68592f150f81468004f7ec311dfc5b
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-23 10:31:58 +0400

    custom component

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
 import { Text, View, Image, ImageBackground, ScrollView, Button, Pressable, Modal, StatusBar, ActivityIndicator, Alert } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
+import Greet from "./components/Greet";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="Press" onPress={() => Alert.alert("Invalid data!")} />
+      <Greet name={"User"} />
+      <Greet name={"Saqi"} />
     </View>
   );
 }
