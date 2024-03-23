287c914b713a9a3703a5cb20302ad8064acd5e46
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-23 17:30:55 +0400

    justify content

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -20,7 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: "column",
+    justifyContent: "center",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
