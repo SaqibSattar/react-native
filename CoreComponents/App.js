2c8d73802c2f8f358c4b3ab58a08437407e9f641
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-23 11:13:52 +0400

    multiple styles

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -4,7 +4,12 @@ import { Text, View, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet ApI</Text>
+      <View style={[styles.box, styles.lightblueBox]}>
+        <Text>LightBlue Box</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBox]}>
+        <Text>LightGreen Box</Text>
+      </View>
     </View>
   );
 }
@@ -12,5 +17,7 @@ export default function App() {
 
 export const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "plum", padding: 60 },
-  title: { fontSize: 16 }
+  box: { width: 100, height: 100, padding: 10 },
+  lightblueBox: { backgroundColor: "lightblue" },
+  lightgreenBox: { backgroundColor: "lightgreen" }
 });
