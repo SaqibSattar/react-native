c9a5f5aacc1d4e777c3e2e28510f305c4c235115
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-23 11:24:45 +0400

    box model

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -17,7 +17,16 @@ export default function App() {
 
 export const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "plum", padding: 60 },
-  box: { width: 100, height: 100, padding: 10 },
+  box: {
+    width: 100,
+    height: 100,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 10,
+    borderWidth: 2,
+    borderColor: "purple",
+    borderRadius: 5
+  },
   lightblueBox: { backgroundColor: "lightblue" },
-  lightgreenBox: { backgroundColor: "lightgreen" }
+  lightgreenBox: { backgroundColor: "lightgreen" },
 });
