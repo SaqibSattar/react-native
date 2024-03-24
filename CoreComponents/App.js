7dab29c5c6660447684b97c31db31dcc131cc9cf
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-24 13:24:49 +0400

    flexWrap

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -6,9 +6,9 @@ export default function App() {
   return (
     // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center"  }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
@@ -20,9 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
