08fbb859fa4e63446bde80ee7072d0fab5fd30b6
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-24 14:36:21 +0400

    flex grow

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -6,9 +6,9 @@ export default function App() {
   return (
     // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f", flexShrink: 1 }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56", flexShrink: 1 }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flexGrow: 2 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f", flexGrow: 1 }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
@@ -20,9 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-start",
-    width: 300,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
