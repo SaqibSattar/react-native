c79134486c476b0ca162b77fadb936c311bfbc78
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-24 14:14:19 +0400

    flex basis

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flexBasis: 140 }}>Box 1</Box>
       <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       {/* <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
@@ -19,10 +19,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    height: 300,
-    flexWrap: "wrap",
-    rowGap: 20,
+    flex: 1,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
