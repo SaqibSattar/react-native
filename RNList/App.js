8f77d55261d8204fe08e8381b68141298c6ae687
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-25 11:38:54 +0400

    new project setup and implement Lists

---
+++ RNList/App.js
@@ -0,0 +1,48 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from "react-native";
+import pokemonList from "./data.json";
+
+export default function App() {
+  return (
+    <SafeAreaView styles={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => {
+          console.log(pokemon.id);
+          return (
+            <View style={styles.card} key={pokemon.id}>
+              <Text style={styles.cardText}>{pokemon.type}</Text>
+              <Text style={styles.cardText}>{pokemon.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    marginBottom: 5,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+});
