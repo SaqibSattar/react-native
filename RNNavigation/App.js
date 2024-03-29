0d6e52fb9100c3a76b79624488dce60aa103f019
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-29 12:28:40 +0400

    Drawer navigation

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -1,63 +1,18 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import AboutScreen from "./screens/AboutScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import DashboardScreen from "./screens/DashboardScreen";
+import SettingsScreen from "./screens/SettingsScreen";
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#6a51ae",
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-          headerRight: () => (
-            <Pressable
-              onPress={() => {
-                alert("Menu button pressed!");
-              }}
-            >
-              <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
-            </Pressable>
-          ),
-          contentStyle: {
-            backgroundColor: "#e8e4f3",
-          },
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "Welcome Home",
-          }}
-        />
-        <Stack.Screen
-          name="About"
-          component={AboutScreen}
-          initialParams={{ name: "Guest" }}
-          // options={({ route }) => ({
-          //   title: route.params?.name,
-          // })}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Dashboard">
+        <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
