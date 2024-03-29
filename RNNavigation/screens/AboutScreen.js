f57eca1ef2af9f9a7c9f22431073a1526ee5ae44
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-29 11:12:57 +0400

    navigation between screens

--- RNNavigation/screens/AboutScreen.js
+++ RNNavigation/screens/AboutScreen.js
@@ -1,9 +1,13 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 
-export default function AboutScreen() {
+export default function AboutScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate("Home")}
+      />
     </View>
   );
 }
--- RNNavigation/screens/HomeScreen.js
+++ RNNavigation/screens/HomeScreen.js
@@ -1,9 +1,16 @@
 import { View, Text, Button, StyleSheet } from "react-native";
+// import { useNavigation } from "@react-navigation/native";
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
+  //   const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Button
+        title="Go to About"
+        onPress={() => navigation.navigate("About")}
+      />
     </View>
   );
 }
