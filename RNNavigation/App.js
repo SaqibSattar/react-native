d3db4fe5ab93478040c0f2e3ae035a60675a226f
SaqibSattar <saqibkhanz1234@gmail.com>
2024-03-30 11:30:54 +0400

    Drawer navigation options

--- RNNavigation/App.js
+++ RNNavigation/App.js
@@ -10,7 +10,19 @@ export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Dashboard">
-        <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+        <Drawer.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            title: "My dashboard",
+            drawerLabel: "Dashboard label",
+            drawerActiveTintColor: "#333",
+            drawerActiveBackgroundColor: "lightblue",
+            drawerContentStyle: {
+              backgroundColor: "#c6cbef",
+            },
+          }}
+        />
         <Drawer.Screen name="Settings" component={SettingsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
